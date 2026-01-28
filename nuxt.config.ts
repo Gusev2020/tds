ad06945341f541dcac781cc8d5859ba038993f0e
Gusev2020 <gusev12.gusev@yandex.ru>
2026-01-28 13:17:09 +0300

    Изменение структуры, измнение путей в конфиге

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: false }
+  devtools: { enabled: false },
+  components: [
+    {
+      path: 'components',
+      pathPrefix: false
+    },
+    {
+      path: 'sdk/components',
+      pathPrefix: false
+    }
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use '@/sdk/assets/styles/variables/colors';
+            @use '@/sdk/assets/styles/variables/layers';
+            @use '@/sdk/assets/styles/variables/transitions';
+          `,
+        }
+      }
+    }
+  },
+  css: [
+    '@/sdk/assets/styles/styles.scss'
+  ]
 })
